806d6ed8d4d7a6f9f8c53028da83451c75bcf6b3
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-01-22 13:23:18 -0800

    to much to mention, not a good job on my part commiting

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-console.log('app.js is running');
+import { getPokedex } from './fetch.js';
+
 const template = document.querySelector('#template');
-const list = document .quarrySelector('#list');
-console.log('elemnts');
+const list = document.querySelector('#list');
+const audio = document.querySelector('#audio');
+
+async function getPokemon() {
+    const pokemons = await getPokedex();  
+    for (let pokemon of pokemons) {
+        const clone = template.content.cloneNode(true);
+        const pokemonName = clone.querySelector('#pokemon');
+        const pokemonImage = clone.querySelector('#image');
+        audio.src = '/assets/pokemon.wav';
+        audio.play();
+
+        pokemonName.textContent = pokemon.pokemon;
+        pokemonImage.src = pokemon.url_image;
+        list.append(clone);
+    }
+
+}
+
+window.addEventListener('load', async() => {
+    await getPokemon();
+
+    
+
+    //if (selected === 'pokemon') {
+        //list.innerHTML = 'pokedex';
+        // await loadPokedex();
+
+    //} else if (selected === 'star-wars') {
+       // list.innerHTML = 'people';
+        //await loadStarWars();
+   // }
+
+});
+
+//console.log('app.js is running');
+//const template = document.querySelector('#template');
+//const list = document .quarrySelector('#list');
+//console.log('elements');
